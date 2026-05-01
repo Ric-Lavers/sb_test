a927c39c137379178c2077ad46a5191ddc76b15d
Ric-Lavers <30609773+Ric-Lavers@users.noreply.github.com>
2026-05-01 15:04:55 +1000

    feat: arnie quote getter

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,9 +1,19 @@
 const { httpGet } = require('./mock-http-interface');
 
-const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
-};
+
+/**
+ * @param {string[]} urls
+ * @return {Promise<Array<{'Arnie Quote': string} | {'FAILURE': string}>>}
+ */
+const getArnieQuotes = async (urls) => 
+  Promise.all(urls.map(httpGet)).then(responses => responses.map(arnieMapper));
+
+const arnieMapper = ({ status, body })  => (
+  message = JSON.parse(body).message,
+  key = status === 200 ? 'Arnie Quote' : 'FAILURE',
+  { [key]:message }
+)
+
 
 module.exports = {
   getArnieQuotes,
